cf9609a5b65e5e3b8abea5ee025473e7bdedce15
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-16 09:44:42 +0700

    pertemuan6

---
+++ pertemuan6/pertemuan6.js
@@ -0,0 +1,28 @@
+let number1 = 10;
+let number2 = 12;
+let number3 = '10';
+let number4 = 'alpukat';
+let number5 = 'Alpukat';
+let number6 = 'cherry';
+let number7 = 10;
+let number8;
+let number9 = null;
+
+
+
+console.log(number8)
+
+console.log(number2 > number1);
+console.log(number1 > number2);
+console.log(number1 < number2);
+console.log(number1 >= number2);
+console.log(number2 >= number1);
+console.log(number1 >= number7);
+console.log(number1 == number3);
+console.log(number1 === number3);
+console.log(number1 != number3)
+console.log(number1 !== number3)
+console.log(number4 > number6)
+console.log(number4 > number5)
+
+
