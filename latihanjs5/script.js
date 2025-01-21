2ef92fe4ca06dc853a27aa2dfa940dacddfa13b3
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-21 10:53:12 +0700

    latihanjs5

--- latihanjs5/script.js
+++ latihanjs5/script.js
@@ -10,6 +10,67 @@ let belanja = 100;
 
 
 
+
+
+
 let harga = 100;
-let diskon = harga >= 100 ? 'Anda mendapatkan diskon 20%' :
-            
+diskon = harga >= 100 ? 'Anda mendapatkan diskon 20%' :
+     harga >= 50 ? 'Anda mendapatkan diskon 10%' :
+            harga < 50 ? 'Anda tidak mendapatkan diskon' : 'Anda tidak mendapat diskon';
+
+
+console.log(diskon)
+
+
+
+
+let nilaiUjian = 81;
+
+    if (nilaiUjian >= 91) {
+        console.log('Grade A');
+    } else if (nilaiUjian >= 81) {
+        console.log('Grade B');
+    } else if (nilaiUjian >= 71) {
+        console.log('Grade C');
+    } else {
+        console.log('Grade D');
+    }
+
+let nilaiSusulan = 81;
+    grade = nilaiSusulan >= 91 ? 'Grade A' :
+                nilaiSusulan >= 81 ? 'Grade B' :
+                    nilaiSusulan >= 71 ? 'Grade C' : 'Grade D' ;
+
+console.log(grade);
+
+let buah = 'Salak dan Nangka';
+switch (buah) {
+    case 'Chery':
+        'Nurudin membeli Chery'
+    break;
+    
+    case 'Nangka':
+        'Nurudin membeli Nangka'
+    break;
+    
+    case 'Salak':
+        'Nurudin membeli Salak'
+    break;
+
+    case 'Salak dan Nangka':
+        'Nurudin membeli Salak dan Nangka'
+    break;
+
+    default:
+        'Nurudin tidak membeli buah apapun'
+
+}
+
+    console.log(buah);
+
+let saklar = undefined;
+if (saklar) {
+    console.log('Saklarnya Menyala');
+} else {
+    console.log('Saklarnya Mati');
+}
