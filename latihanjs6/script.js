ff19545a08496f504a60f326463984bc69251123
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-21 13:37:21 +0700

    latihanjs6

---
+++ latihanjs6/script.js
@@ -0,0 +1,29 @@
+//latihan 9
+
+let nilaiMatematika = 91;
+let nilaiAlam = 81;
+let nilaiSosial = 91;
+
+if (nilaiMatematika >= 91 && nilaiAlam >= 91 && nilaiSosial >= 91) {
+    console.log('Anda Lulus')
+} else {
+    console.log('Anda tidak lulus')
+}
+
+
+let nilaiMTK = 91;
+let nilaiIPA = 81;
+let nilaiIPS = 91;
+
+if (nilaiMTK >= 91 || nilaiIPA >= 91 || nilaiIPS >= 91) {
+    console.log('Anda Lulus')
+} else {
+    console.log('Anda tidak Lulus')
+}
+
+let isLoggedIn = true;
+if (isLoggedIn) {
+    console.log('Anda sudah login')
+} else {
+    console.log('Anda belum login')
+}
