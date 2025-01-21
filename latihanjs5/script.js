4e6142e3d6b5214a17e62eeba9126150b565ebc9
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-21 09:46:50 +0700

    latihanjs5

---
+++ latihanjs5/script.js
@@ -0,0 +1,15 @@
+let belanja = 100;
+
+    if (belanja >= 100) {
+        console.log('Anda mendapatkan diskon 20%')
+    } else if (belanja >= 50) {
+        console.log('Anda mendapatkan diskon 10%')
+    } else if(belanja < 50) {
+        console.log('Maaf, anda belum dapat diskon')
+    }
+
+
+
+let harga = 100;
+let diskon = harga >= 100 ? 'Anda mendapatkan diskon 20%' :
+            
