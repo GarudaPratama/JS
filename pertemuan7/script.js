e352123682f8471a30ac13cce257c0bed53a9d65
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-21 09:46:43 +0700

    pertemuan7

--- pertemuan7/script.js
+++ pertemuan7/script.js
@@ -1,22 +1,22 @@
-        let getElement = document .getElementById('contohelement');
-        console.log(getElement.innerText);//utk melihat list
-        console.dir(getElement);//utk melihat direktori
-        console.log(getElement.baseURI);//buat cek URL
-        getElement.innerText = 'Sekolah Tahfizh Bogor';//buat update
+        // let getElement = document.getElementById('contohelement');
+        // console.log(getElement.innerText);//utk melihat list
+        // console.dir(getElement);//utk melihat direktori
+        // console.log(getElement.baseURI);//buat cek URL
+        // getElement.innerText = 'Sekolah Tahfizh Bogor';//buat update
 
 
         //membuat elemen baru
-        let h1Element = document.createElement('h1');
-        //isi elemen baru
-        h1Element.innerText = 'ini elemen baru'
-        //sisipkan elemen baru
-        document.getElementById('container').appendChild(h1Element);
+        // let h1Element = document.createElement('h1');
+        // //isi elemen baru
+        // h1Element.innerText = 'ini elemen baru'
+        // //sisipkan elemen baru
+        // document.getElementById('container').appendChild(h1Element);
 
 
 
         //value pada input
-        let inputElement = document.getElementById('age');
-        console.dir(inputElement);
+        // let inputElement = document.getElementById('age');
+        // console.dir(inputElement);
 
 
         function myFunction() {
@@ -36,7 +36,7 @@
         }
 
 
-        let nilaiMatematika = 80;
+        let nilaiMatematika = 99;
         
 
         if (nilaiMatematika >= 90) {
@@ -52,3 +52,54 @@
             console.log('Grade D')
         }
         
+
+
+        let cetekan = 0;
+        if (cetekan) {
+            console.log ('hidup');
+        } else {
+            console.log ('mati');
+        }
+
+
+        cetekan ? console.log('hidup') : console.log('mati')
+
+
+
+        let number2 = 11;
+        number2 >= 10 ? console.log('angkanya 10 atau lebih') : console.log ('angkanya kurang dari 10');
+
+
+        //multiple ternary
+        let number3 = 90;
+        number3 >= 90 ? console.log('Grade A') :
+             number3 >= 80 ? console.log('Grade B') :
+                number3 >= 70 ? console.log ('Grade C') :
+                    number3 >= 60 ? console.log('Grade D') :
+                        console.log('Grade E');
+
+
+
+        //penggunaan ? yang non tradisional
+        let isLogin = true;
+        
+        //contoh 1
+        isLogin ? console.log('Selamat datang') : console.log('Anda belum LogIn')
+
+        //contoh 2
+        let pesan = isLogin ? 'Selamat datang' : 'Anda belum LogIn';
+        console.log(pesan)
+
+        //switch statement
+        let number4 = 100;
+        switch(number4) {
+            case 100:
+            case 90:
+                console.log('Grade A');
+                break;
+            case 80:
+                console.log('Grade B');
+                break;
+            default:
+                console.log('Grade E')
+        }
