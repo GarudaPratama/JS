5dda75eb73240cb279ee3745086d29e8395aa4d7
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-24 08:15:22 +0700

    kalkulatorcuy

--- kalkulatorcuy/kalkulator.js
+++ kalkulatorcuy/kalkulator.js
@@ -1,24 +1,58 @@
 function numberOne() {
-    let getElement = document.getElementById('hasil').innerText;
+    let getElement = document.getElementById('hasil').innerHTML;
+    if (getElement == '0'){
+        getElement = '';
+    }
     let total = getElement + 1;
-    document.getElementById('hasil').innerText = total;
+    document.getElementById('hasil').innerHTML = total;
 }
 
 function numberTwo() {
-    let getElement = document.getElementById('hasil').innerText;
+    let getElement = document.getElementById('hasil').innerHTML;
+    if (getElement == '0'){
+        getElement = '';
+    }
     let total = getElement + 2;
-    document.getElementById('hasil').innerText = total;
+    document.getElementById('hasil').innerHTML = total;
 }
 
 function tambah() {
-    let getElement = document.getElementById('hasil').innerText;
+    let getElement = document.getElementById('hasil').innerHTML;
     let total = getElement + '+';
-    document.getElementById('hasil').innerText = total;
+    document.getElementById('hasil').innerHTML = total;
 
+}
+
+function kurang() {
+    let getElement = document.getElementById('hasil').innerHTML;
+    let total = getElement + '-';
+    document.getElementById('hasil').innerHTML = total;
 }
 
 function total() {
-    let getElement = document.getElementById('hasil').innerText;
-    let total = getElement + '=';
-    document.getElementById('hasil').innerText = total;
+    let getElement = document.getElementById('hasil').innerHTML;
+    console.log(getElement);
+
+    if (getElement.includes("+")) {
+        let parts = getElement.trim().split("+");
+        console.log(parts);
+        let operand1 = Number(parts[0]);
+        let operand2 = Number(parts[1]);
+
+        let total = operand1 + operand2;
+        console.log(total);
+        document.getElementById('hasil').innerHTML = total;
+    } else if (getElement.includes("-")) {
+        let parts = getElement.trim().split("-");
+        console.log(parts);
+        let operand1 = Number(parts[0]);
+        let operand2 = Number(parts[1]);
+
+        let total = operand1 - operand2;
+        console.log(total);
+        document.getElementById('hasil').innerHTML = total;
+    }
+
+    
 }
+
