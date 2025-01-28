33e4884d3c104a7375ffd3c0997a4b5275479850
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-28 09:51:44 +0700

    pertemuan10

--- pertemuan10 - looping/script.js
+++ pertemuan10 - looping/script.js
@@ -7,7 +7,29 @@ while (i <= 5) {
 
 
 let j = 1;
+
 do {
     console.log(j);
     j++;
+} while (j <= 5)
+
+
+//! for ()
+for (let k = 1; k <= 5; k++){
+    console.log(k);
+}
+
+for (let l = 1; l <= 5; l++){
+    if (l === 5) continue;
+    console.log(l);
+}
+
+//menghentikan perulangan (break)
+for (let m = 1; m <= 5; m++){
+    if (m === 4) break;
+    console.log(m);
+}
+
+for (let n = 10; n >= 4; n--){
+    console.log(n);
 }
