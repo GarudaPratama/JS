fa3bc1daaa2ea50251e4282e067acd557e73f785
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-28 10:39:35 +0700

    latihan7

---
+++ latihanjs7/script.js
@@ -0,0 +1,28 @@
+let b = 1;
+
+while (b <= 10){
+    console.log(b);
+    b++;
+}
+
+let p = 1
+
+do {
+    console.log(p);
+    p++;
+} while (p <= 10)
+
+for (let l = 2; l <= 20; l += 2){
+    console.log(l);
+}
+
+for (let k = 1; k <= 10; k++){
+    if (k === 5) continue;
+    console.log(k);
+}
+
+for (let j = 1; j <= 10; j++){
+    if (j === 6) break;
+    console.log(j);
+}
+
