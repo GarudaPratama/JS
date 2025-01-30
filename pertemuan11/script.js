6cd683dadd4b001fb4ea29ffb2f45e37b21e736d
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-01-30 11:39:07 +0700

    pertemuan11

--- pertemuan11/script.js
+++ pertemuan11/script.js
@@ -31,9 +31,12 @@ console.log(firstName);
 //!Parameter
 //*Parameter adalah input yang dikirimkan ke fungsi
 
-function angka(nilai){
+function angka(nilai, nilai2){
     console.log(nilai);
+    console.log(nilai2);
 }
+
+angka(45, 90);
 
 
 //! Nilai Default
@@ -44,4 +47,40 @@ function number(no = 0){
     
 
 }
-number();
+number(9);
+
+//! alternatif nilai default parameter
+function lastName(name) {
+    console.log(
+        name = name || "Abdurrahman"
+    )
+}
+lastName();
+
+//* case 2 menggunakan ??
+function nickName(nick) {
+    nick = nick ?? "Supercoder";
+
+    console.log(nick);
+}
+nickName();
+
+//!mengembalikan nilai (return)
+//* fungsi dapat mengembalikan nilai dengan menggunakan kata kunci 'return'
+function perkalian(a, b){
+    return a * b;
+}
+let hasilPerkalian = perkalian(2, 3);
+console.log(hasilPerkalian);
+
+function cetakTwo(){
+    console.log('cetakTwo');
+}
+cetakTwo();
+
+function add(a, b) {
+    return a + b;
+    }
+    console.log(add(3, 5));
+
+    
