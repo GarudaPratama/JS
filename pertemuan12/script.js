777dc6523d7fc76dba5a4f48aad4404ee2ffdfdd
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-02-06 10:38:56 +0700

    pertemuan12

--- pertemuan12/script.js
+++ pertemuan12/script.js
@@ -66,3 +66,21 @@ let sayHello2 = function(){
 }
 sayHello2();
 
+
+    function penambahan(a, b) {
+    
+    }
+     
+    function perkalian(a, b) {
+    
+    }
+     
+    // Fungsi utama yang menerima callback
+    function calculate(num1, num2, operation) {
+        let result = operation(num1, num2);
+        console.log("Hasil:", result);
+    }
+     
+    // pemanggilan fungsi
+    calculate(5, 3, penambahan); // Output: Hasil: 8
+    calculate(4, 2, perkalian); // Output: Hasil: 8
