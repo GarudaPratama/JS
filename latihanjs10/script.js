8e89a997162b9cfebc6d2c3c042d6e3919f582c3
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-02-07 09:04:33 +0700

    latihanjs10

---
+++ latihanjs10/script.js
@@ -0,0 +1,15 @@
+
+//! Deklarasi
+function calculateArea(length, width) {
+    return length * width;
+   }
+
+//! Ekspresi
+let calculateArea = function (length, width) {
+    return length * width;
+}
+
+//! Arrow Function
+let calculateArea = (length, width) => {
+    return length * width;
+}
