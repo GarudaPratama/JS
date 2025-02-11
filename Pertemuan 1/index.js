7399be35445a190f6c0fdb2d1c6d6347fc79a74a
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-02-11 11:21:40 +0700

    pertemuan1

--- Pertemuan 1/index.js
+++ Pertemuan 1/index.js
@@ -1,1 +1,14 @@
-alert('Hello World');
+
+
+console.log(2 ** 3);
+
+console.log(Boolean("false"));
+
+console.log(2 + "2" - 1);
+
+console.log(typeof null);
+
+let x = 5;
+let y = x++;
+console.log(y);
+
