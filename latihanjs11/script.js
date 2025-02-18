404c588e02e9388ab0d25f0cf397f32dc3a3701e
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-02-18 11:20:14 +0700

    latihanjs11

---
+++ latihanjs11/script.js
@@ -0,0 +1,25 @@
+//! Tugas 1
+//* 1.1 Membuat object
+
+let mahaSiswa = {
+    nama: "Andi",
+    umur: 21,
+    jurusan: "Teknik Informatika",
+    ipk: 3.75,   
+}
+
+//* 1.2 Menambahkan properti
+mahaSiswa.email = "andi.pratama@gmail.com";
+
+//* 1.3 mengambil data dengan destructuring
+const {nama, jurusan} = mahaSiswa;
+console.log(nama)
+console.log(jurusan)
+
+//* 1.4 Menampilkan seluruh object mahasiswa
+console.log(mahaSiswa);
+
+
+//! Tugas 2
+
+
